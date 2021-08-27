4991f4a7cd2a5202ac4bd43072531fa1d1aff08c
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-08-27 20:28:11 -0300

    atualizando a rota e o controller sobre

--- controllers/sobreCtrl.js
+++ controllers/sobreCtrl.js
@@ -1,25 +1,71 @@
 
+const sobreSchema = require('../models/sobreModel');
 
-exports.getSobre =  (req,res)=>{
-    res.send('sobre específico post router')
-}
 
-exports.addSobre =  (req,res)=>{
-    res.send('sobre específico post router')
+exports.getSobre = async (req,res)=>{
+    const sobre = await sobreSchema.find();
+    try {
+        res.json(sobre);
+    } catch (error) {
+        res.status(500).json({msg: 'srver prblem'});
+    }
 }
+    
+exports.addSobre = async (req,res)=>{
+    const {sobre} = req.body;
+    try {
+        const newSobre = new sobreSchema({
+         
+            sobre
+        })
+        await newSobre.save();
+        res.json(newSobre);
+    } catch (error) {
+        res.status(500).json({msg: 'srver prblem'});
+    }
+}
+    
+exports.getSobreId =  async (req, res)=>{
 
-exports.getSobre =  (req,res)=>{
-    res.send('sobre específico post router')
-}
+    try {
+      const sobre= await sobreSchema.findById(req.params.id);
+    res.json(sobre);
+    
+    } catch (error) {
+      res.status(500).json({msg:'server problem'})
+    }
+    
+    
+    }
 
-exports.getSobreId =  (req,res)=>{
-    res.send('sobre específico post router')
-}
+    exports.updateSobre = async (req, res)=>{
+        const {sobre} = req.body;
+       
+        try {
+         const newSobre = await sobreSchema.findByIdAndUpdate(req.params.id, {
+      
+           sobre
+       
+          });
+       
+          let results = await newSobre.save();
+          await results;
+          res.json({msg:'Item atualizado'})
+          
+        } 
+        catch (error) {
+         res.status(500).json({msg:'server problem'})
+        }
+      
+      
+      
+      }
 
-exports.updateSobre =  (req,res)=>{
-    res.send('atualizando específico post router')
-}
-
-exports.delSobre =  (req,res)=>{
-    res.send('excluindo específico post router')
-}
+  exports.delSobre = async (req, res)=>{
+    const sobre = await sobreSchema.findByIdAndDelete(req.params.id)
+ 
+    sobre;
+ 
+    res.json({msg:"Item deletado"})
+ 
+ }
--- routes/sobreRoute.js
+++ routes/sobreRoute.js
@@ -1,69 +1,24 @@
 const router = require('express').Router();
-//const {getSobre, getSobreId, addSobre, updateSobre, delSobre} = require('../controllers/sobreCtrl');
+const {getSobre, addSobre, getSobreId, updateSobre, delSobre} = require('../controllers/sobreCtrl');
 
 const sobreSchema = require('../models/sobreModel');
 
 //get sobre
-router.get('/sobre', async(req, res)=>{
-const sobre = await sobreSchema.find();
-try {
-    res.json(sobre);
-} catch (error) {
-    res.status(500).json({msg: 'srver prblem'});
-}
-
-})
+router.get('/sobre', getSobre);
 
 //ADD SOBRE USER
-router.post('/sobre',async (req, res)=>{
-    const {sobre} = req.body;
-try {
-    const newSobre = new sobreSchema({
-        sobre
-    })
-    await newSobre.save();
-    res.json(newSobre);
-   
-} catch (error) {
-    res.status(500).json({msg: 'srver prblem'});
-}
-
-})
+router.post('/sobre',addSobre);
   
 
 //get específico user pelo id
-router.get('/sobre/:id',async (req,res)=>{
-try {
-    const sobre = await sobreSchema.findById(req.params.id);
-    res.json(sobre);
-} catch (error) {
-    res.status(500).json({msg: 'srver prblem'});
-}
-})
+router.get('/sobre/:id', getSobreId);
 
 
 //atualizando específico user pelo id
-router.put('/sobre/update/:id', async(req,res)=>{
-const {sobre} = req.body;
-try {
-    const newSobre = await sobreSchema.findByIdAndUpdate(req.params.id,{
-        sobre
-    });
-    let results = await newSobre.save();
-    await results;
-    res.json({msg: 'Atualizado'})
-} catch (error) {
-
-    res.status(500).json({msg: 'srver prblem'})
-}
-})
+router.put('/sobre/update/:id', updateSobre);
 
 //excluindo específico user pelo id
-router.delete('/sobre/:id',async (req,res)=>{
-const sobre = await sobreSchema.findByIdAndDelete(req.params.id);
-sobre;
-res.status(500).json({msg: 'excluído com sucess'})
-})
+router.delete('/sobre/:id',delSobre);
 
 
 

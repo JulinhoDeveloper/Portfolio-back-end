cc44147f79a79c23415c97ef0012eae813c960be
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-08-28 18:32:25 -0300

    educacao

---
+++ models/educacaoModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const educacaoSchema = new mongoose.Schema({
+    educacao:{
+        type:String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('educacao', educacaoSchema);

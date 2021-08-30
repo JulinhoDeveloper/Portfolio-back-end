7223a2ddf37d4cafd8be4b945ee2a539fda415ef
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-08-30 15:36:55 -0300

    rotas

--- routes/contatoRoute.js
+++ routes/contatoRoute.js
@@ -35,7 +35,7 @@ smtpTransport.sendMail(mailOptions, (err, response)=>{
     try {
         if(err) return res.status(400).json({msg:err})
         else{
-            return res.status(200).json({msg:`Sua mensagem foi enviada`})
+            return res.status(200).json({msg:`A sua mensagem foi enviada`})
         }
 
     } catch (err) {

b913ca2a54e40cb9a251fc0baa4287687755bdf9
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-08-30 08:07:34 -0300

    rotas

---
+++ routes/contatoRoute.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const nodemailer = require('nodemailer');
+
+router.get('/', (req,res)=>{
+    res.send('contato')
+})
+
+router.post('/', (req, res)=>{
+    let data = req.body;
+let smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port:465,
+    auth:{
+        user:'juliodevfullstack@gmail.com',
+        pass:'cachorrinho'
+    }
+})
+
+let mailOptions = {
+    from:data.email,
+    to:'juliodevfullstack@gmail.com',
+    pass:'cachorrinho',
+    subject:`Message from ${data.name}`,
+    html:`<h3>Informação</h3>
+    <ul>
+    <li>Nome: ${data.name}</li>
+    <li>email: ${data.email}</li>
+    </ul>
+    <h3>Mensagem</h3>
+    <p>${data.message}</p>    
+    `
+}
+
+smtpTransport.sendMail(mailOptions, (err, response)=>{
+    try {
+        if(err) return res.status(400).json({msg:err})
+        else{
+            return res.status(200).json({msg:`Sua mensagem foi enviada`})
+        }
+
+    } catch (err) {
+        res.status(500).json({msg:err})
+    }
+})
+
+
+});
+
+
+
+
+module.exports = router;
